764de4914349e606aa64fae8461aa7a3cc8af485
aldrinbgama <aldrin.gama@umuzi.org>
2020-02-12 10:19:01 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        PerfectlyNormalDice perfectlyNormalDice = new PerfectlyNormalDice();
+        System.out.println(perfectlyNormalDice.Dice());
+    }
+}
---
+++ src/main/java/PerfectlyNormalDice.java
@@ -0,0 +1,20 @@
+public class PerfectlyNormalDice {
+    public int Dice() {
+        double rand = Math.random();
+        int roll;
+
+        if (rand < 1.0/8.0) {
+            roll = 1;
+        } else if (rand < 2.0/8.0) {
+            roll = 2;
+        } else if (rand < 3.0/8.0)
+            roll = 3;
+        else if (rand < 4.0/8.0)
+            roll = 4;
+        else if (rand < 5.0/8.0) {
+            roll = 5;
+        } else
+            roll = 6;
+        return roll;
+    }
+}
