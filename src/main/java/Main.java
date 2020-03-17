2244a921cebcb980176f266a55d803e5f7978cb7
aldrinbgama <aldrin.gama@umuzi.org>
2020-03-17 03:24:02 +0200

    2nd commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        PerfectlyNormalDice perfectlyNormalDice = new PerfectlyNormalDice();
-        System.out.println(perfectlyNormalDice.Dice());
+        System.out.println("Perfectly Normal Dice.");
+        Die dice6 = new Die(6);
+        dice6.roll();
+        System.out.println(dice6.value);
+
+        System.out.println("Twenty Sided Dice.");
+        Die dice20 = new Die(20);
+        dice20.roll();
+        System.out.println(dice20.value);
+
+        System.out.println("Weighted Dice.");
+        Die weighted = new Die(6, new int[]{1, 1, 1, 1, 1, 2});
+        weighted.roll();
+        System.out.println(weighted.value);
     }
 }
